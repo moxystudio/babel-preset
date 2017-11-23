f3535084427d95a7df2651e0096a51e207487c9c
André Cruz <andremiguelcruz@msn.com>
2017-11-23 09:30:05 +0000

    feat: implement namedDefaultExport
    BREAKING CHANGE: browser target was changed to be browsers for consistency with babel-preset-env
    
    Closes #5.

--- test/index.spec.js
+++ test/index.spec.js
@@ -5,34 +5,38 @@ const preset = require('..');
 
 expect.addSnapshotSerializer(serializerPath);
 
-it('should return a default config that target browsers and node', () => {
-    expect(preset()).toMatchSnapshot();
+describe('targets', () => {
+    it('should return a default config that target browsers and node', () => {
+        expect(preset()).toMatchSnapshot();
+    });
+
+    it('should return a config for node if options.targets is set to node', () => {
+        expect(preset(null, { targets: ['node'] })).toMatchSnapshot();
+    });
+
+    it('should return a config for browsers if options.targets is set to browsers', () => {
+        expect(preset(null, { targets: ['browsers'] })).toMatchSnapshot();
+    });
+
+    it('should allow overriding options.targets with an object', () => {
+        expect(preset(null, { targets: { node: 'current' } })).toMatchSnapshot();
+    });
 });
 
-it('should return a config for node if options.targets is set to node', () => {
-    expect(preset(null, { targets: ['node'] })).toMatchSnapshot();
-});
+describe('modules', () => {
+    it('should set modules to false if options.modules is disabled', () => {
+        expect(preset(null, { modules: false })).toMatchSnapshot();
+    });
 
-it('should return a config for node if options.targets is set to browser', () => {
-    expect(preset(null, { targets: ['browsers'] })).toMatchSnapshot();
-});
+    it('should set modules to false by default if BABEL_ENV is es', () => {
+        process.env.BABEL_ENV = 'es';
 
-it('should allow overriding options.targets with an object', () => {
-    expect(preset(null, { targets: { node: 'current' } })).toMatchSnapshot();
-});
-
-it('should disable modules if options.modules is disabled', () => {
-    expect(preset(null, { modules: false })).toMatchSnapshot();
-});
-
-it('should set modules to false if BABEL_ENV is es', () => {
-    process.env.BABEL_ENV = 'es';
-
-    try {
-        expect(preset(null)).toMatchSnapshot();
-    } finally {
-        delete process.env.BABEL_ENV;
-    }
+        try {
+            expect(preset(null)).toMatchSnapshot();
+        } finally {
+            delete process.env.BABEL_ENV;
+        }
+    });
 });
 
 describe('react', () => {
@@ -40,7 +44,34 @@ describe('react', () => {
         expect(preset(null, { react: true })).toMatchSnapshot();
     });
 
-    it('should enable react optimizations if env is production', () => {
+    it('should enable react optimizations if options.env is production', () => {
         expect(preset(null, { env: 'production', react: true })).toMatchSnapshot();
+    });
+});
+
+describe('namedDefaultExport & babel-plugin-add-module-exports', () => {
+    it('should enable add-module-exports by default', () => {
+        expect(preset(null)).toMatchSnapshot();
+    });
+
+    it('should enable add-module-exports if options.modules is commonjs', () => {
+        expect(preset(null)).toMatchSnapshot();
+        expect(preset(null, { modules: 'commonjs' })).toMatchSnapshot();
+    });
+
+    it('should not enable add-module-exports if options.modules is not commonjs', () => {
+        expect(preset(null, { modules: false })).toMatchSnapshot();
+    });
+
+    it('should respect options.namedDefaultExport', () => {
+        expect(preset(null, { namedDefaultExport: true })).toMatchSnapshot();
+        expect(preset(null, { namedDefaultExport: false })).toMatchSnapshot();
+        expect(preset(null, { modules: 'commonjs', namedDefaultExport: true })).toMatchSnapshot();
+        expect(preset(null, { modules: 'commonjs', namedDefaultExport: false })).toMatchSnapshot();
+    });
+
+    it('should throw when enabling options.namedDefaultExport and options.modules is not commonjs', () => {
+        expect(() => preset(null, { modules: false, namedDefaultExport: true }))
+        .toThrow('The `namedDefaultExport` option can only be enabled when `modules` is commonjs');
     });
 });
